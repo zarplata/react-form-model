5e1d956f6695bc3859ae6138eb4522cc7c849d7f
Communist <apvnsk@gmail.com>
2017-09-03 22:17:35 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+import omit from 'lodash/omit';
+
+export default (formModelSchema, mapToForm, mapFromForm) => WrappedComponent => class Connector extends Component {
+    static displayName = `connector(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+    static WrappedComponent = WrappedComponent;
+
+    constructor(props) {
+        super();
+        const { data } = props;
+
+        this.form = new FormModel(formModelSchema, this.update);
+        this.mapper = {
+            toForm: mapToForm,
+            fromForm: mapFromForm
+        };
+
+        this.form.setData(this.mapper.toForm(data));
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { data } = nextProps;
+
+        if (data !== this.props.data) {
+            this.form.setData(this.mapper.toForm(data));
+        }
+    }
+
+    validateField = () => {
+        const isValid = this.form.validate();
+
+        this.forceUpdate();
+
+        return isValid;
+    };
+
+    handleSubmit = () => {
+        const { onSubmitSuccess, onSubmitFailed } = this.props;
+
+        if (this.form.validate()) {
+            onSubmitSuccess(this.mapper.fromForm(this.form.getData()));
+        } else if (onSubmitFailed) {
+            onSubmitFailed(this.form.getInvalidFields());
+        }
+
+        this.update();
+    }
+
+    handleReset = (shouldValidate = false) => {
+        const { data } = this.props;
+
+        this.form._resetInvalidFields();
+        this.form.setData(this.mapper.toForm(data));
+
+        if (shouldValidate) {
+            this.form.validate();
+        }
+
+        this.forceUpdate();
+    }
+
+    update = () => {
+        this.forceUpdate();
+    }
+
+    render() {
+        return (
+            <WrappedComponent
+                {...omit(this.props, ['onSubmitSuccess', 'onSubmitFailed', 'data'])}
+                form={this.form.getData()}
+                invalidFields={this.form.getInvalidFields()}
+                onSubmit={this.handleSubmit}
+                onReset={this.handleReset}
+                validate={this.validateField}
+            />
+        );
+    }
+};
